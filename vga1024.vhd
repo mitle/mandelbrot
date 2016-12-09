97b302caad5316ca2b9d9e5010523a377da44f09
mitle <mitnyik.levente@hallgato.ppke.hu>
2016-12-10 00:04:45 +0100

    Update vga1024.vhd
    csilivili VGA

--- vga1024.vhd
+++ vga1024.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/25/2016 02:18:26 PM
--- Design Name: 
--- Module Name: vga1024 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity vga1024 is
 Port (    red       : out STD_LOGIC_VECTOR (3 downto 0) := "0000";
@@ -185,8 +156,6 @@ begin
         end if;
     end if;
 end process;
-
-
 
 
 end Behavioral;
