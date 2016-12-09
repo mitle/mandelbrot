a0d190eebfc50f9d5c49e24af1fb70587ac9bc2c
mitle <mitnyik.levente@hallgato.ppke.hu>
2016-12-10 00:00:14 +0100

    Update vga.vhd

--- vga.vhd
+++ vga.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/11/2016 03:52:26 PM
--- Design Name: 
--- Module Name: vga - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity vga is
     Port ( red       : out STD_LOGIC_VECTOR (3 downto 0) := "0000";
@@ -156,6 +127,5 @@ begin
         end if;
     end if;
 end process;
-
 
 end Behavioral;
